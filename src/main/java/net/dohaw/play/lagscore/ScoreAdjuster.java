19712628159ff719d7c6520cad7ed5326ee04da9
Caleb Owens <caleb.ja.owens@gmail.com>
2020-08-22 21:55:32 -0400

    Made TPSUtils

--- src/main/java/net/dohaw/play/lagscore/ScoreAdjuster.java
+++ src/main/java/net/dohaw/play/lagscore/ScoreAdjuster.java
@@ -1,11 +1,16 @@
 package net.dohaw.play.lagscore;
 
+import net.dohaw.play.lagscore.playerdata.PlayerData;
+import net.dohaw.play.lagscore.playerdata.PlayerDataHolder;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.UUID;
 
 public class ScoreAdjuster extends BukkitRunnable {
 
     private Storage storage;
+    private PlayerDataHolder playerDataHolder;
 
     public ScoreAdjuster(Storage storage){
         this.storage = storage;
@@ -14,7 +19,8 @@ public class ScoreAdjuster extends BukkitRunnable {
     @Override
     public void run() {
         for(Player player : storage.plugin.getServer().getOnlinePlayers()){
-
+            UUID uuid = player.getUniqueId();
+            PlayerData pd = playerDataHolder.getPlayerData(uuid);
         }
     }
 
